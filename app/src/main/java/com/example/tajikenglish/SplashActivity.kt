6c36638e82656dd41b08b247812e7791c70966b7
Qalay <qalay84@gmail.com>
2021-10-22 17:36:56 +0500

    Add App_Class

---
+++ app/src/main/java/com/example/tajikenglish/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tajikenglish
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.example.tajikenglish.core.di.ApplicationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+
+        val iv_note: ImageView = findViewById(R.id.iv_note)
+        iv_note.alpha = 0f
+        iv_note.animate().setDuration(2500).alpha(1f).withEndAction {
+
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
