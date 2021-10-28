71ed9cc28562266d69e27dde71420dd0ee4dc672
Qalay <qalay84@gmail.com>
2021-10-28 15:58:24 +0500

    Библиотекахои нодаркора удалить шуд

--- app/src/main/java/com/example/tajikenglish/modules/draw_alphabet/DrawActivity.kt
+++ app/src/main/java/com/example/tajikenglish/modules/draw_alphabet/DrawActivity.kt
@@ -1,31 +1,10 @@
 package com.example.tajikenglish
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Dialog
-import android.app.ProgressDialog
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.media.MediaScannerConnection
-import android.os.AsyncTask
+
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.get
 
-import kotlinx.android.synthetic.main.activity_draw.*
-import kotlinx.android.synthetic.main.dialog_brush_size.*
-
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
 
 class DrawActivity : AppCompatActivity() {
 
