319c4ce4c1de82adc5d7a7db9e29daa55d6baac4
Qalay <qalay84@gmail.com>
2021-11-02 17:00:45 +0500

    Главный экранба иконка шуд

--- app/src/main/java/com/example/tajikenglish/SplashActivity.kt
+++ app/src/main/java/com/example/tajikenglish/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.example.tajikenglish
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 import com.example.tajikenglish.core.di.ApplicationModule
 import org.koin.android.ext.koin.androidContext
@@ -11,9 +13,10 @@ import org.koin.core.context.startKoin
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash)
-
-
 
 
         val iv_note: ImageView = findViewById(R.id.iv_note)
